864da065539c6c5f3d58c95a44923fb21e92bce4
Zhangwentao <15907645468@163.com>
2019-04-23 22:02:02 +0800

    提供代码片段

---
+++ src/components/choice.vue
@@ -0,0 +1,117 @@
+<template>
+<div class=''>
+    <!-- {
+	// Place your snippets for vue here. Each snippet is defined under a snippet name and has a prefix, body and 
+	// description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
+	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. Placeholders with the 
+	// same ids are connected.
+	// Example:
+	// "Print to console": {
+	// 	"prefix": "log",
+	// 	"body": [
+	// 		"console.log('$1');",
+	// 		"$2"
+	// 	],
+	// 	"description": "Log output to console"
+	// }
+	"vue all s": {
+        "prefix": "vue",
+        "body": [
+			"<template>",
+				"<div class=''>",
+
+				"</div>",
+			"</template>",
+
+			"<script>",
+				"export default {",
+				"name:'',",
+				"data () {",
+				" return {",
+					"}",
+				"},",
+				"//实例组件",
+				"components: {},",
+				"//创建vue时的钩子",
+				" created(){},",
+				"//挂载vue时的钩子",
+				"mounted(){},",
+				"//当前vue使用的函数",
+				"methods:{},",
+				"}",
+			"</script>",
+			"<style></style> "
+        ],
+        "description": "生成vue模板结构"
+    },
+	"head": {
+		"prefix": "h",
+		"body": [
+			"<frHead  title='$1' @leftClick='$router.back(-1)' class='black'/>"
+		],
+		"description": "生成头部"
+	},
+	"foot": {
+		"prefix": "f",
+		"body": [
+			"<frFoot  bgColor='$1' height='4rem'>","</frFoot>"
+		],
+		"description": "生成底部部"
+	},
+	"cap": {
+		"prefix": "capfull",
+		"body": [
+			"<div class='fr-cap mb full mt-xs'>$1</div>"
+		],
+		"description": "生成满屏胶囊按钮"
+	},
+	"box files": {
+		"prefix": "boxa",
+		"body": [
+			"<div class='box box-ac'>",
+				"<div class='box-f1'>$1</div>",
+				"<div class='box-f1'>$2</div>",
+        	"</div>"
+		],
+		"description": "生成盒子模型基础版"
+	},
+	"cadr": {
+		"prefix": "cadr",
+		"body": [
+			"<div class='fr-cadr pa-m bs-b mb-s'></div>$1",
+			
+		],
+		"description": "生成卡片模块"
+	},
+	"wat": {
+		"prefix": "wat",
+		"body": [
+			"watch:{$1:{\n handler(cval,oval){\n $2 \n},deep:true}},",
+			
+		],
+		"description": "生成卡片模块"
+	},
+
+	
+	
+} -->
+</div>
+</template>
+<script>
+export default {
+name:'',
+data () {
+ return {
+}
+},
+//实例组件
+components: {},
+//创建vue时的钩子
+ created(){},
+//挂载vue时的钩子
+mounted(){},
+//当前vue使用的函数
+methods:{},
+}
+</script>
+<style></style> 
